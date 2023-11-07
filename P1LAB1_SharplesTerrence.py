38df24f2a0cdb99f8eba4d072703b965e06b23e7
TerrenceJS15 <150169262+TerrenceJS15@users.noreply.github.com>
2023-11-07 11:22:04 -0500

    First Upload

---
+++ P1LAB1_SharplesTerrence.py
@@ -0,0 +1,6 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Sharplet9066/Downloads/P1LAB1_SharplesTerrence.py
+Please enter your name: Terrence Sharples
+Hello Terrence Sharples welcome to CTI-110!
