28339172051ca2788d49bb3fcbb074e4eee27162
TerrenceJS15 <150169262+TerrenceJS15@users.noreply.github.com>
2023-11-09 11:05:26 -0500

    Add files via upload

---
+++ P1HW1_SharplesTerrence.py
@@ -0,0 +1,18 @@
+#Name
+#Date
+#Using intergers and math expressions
+
+#Get input from user
+number1 = int(input("Enter a whole number: "))
+
+#Display the output
+print("You enter: ", number1)
+print(number1, "squared is", number1**2)
+print(number1, "cubed is", number1**3, "!!!")
+
+#Get input from user
+number2 = int(input("Emter another integer: "))
+
+#Display output
+print(number1, "+", number2, "is", number1 + number2)
+print(number1, "*", number2, "is", number1 * number2)
