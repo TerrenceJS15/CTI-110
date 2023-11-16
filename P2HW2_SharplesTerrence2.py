0e181dd3bf8ebf1c9326f3aa04f5f9436662e3fe
TerrenceJS15 <150169262+TerrenceJS15@users.noreply.github.com>
2023-11-16 11:37:25 -0500

    Add files via upload

---
+++ P2HW2_SharplesTerrence2.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/sharplet9066/Downloads/P2HW2_SharplesTerrence.py
+Enter grade for Module 1:65.5
+Enter grade for Module 2:88
+Enter grade for Module 3:78.5
+Enter grade for Module 4:90
+Enter grade for Module 5:61
+Enter grade for Module 6:92
+[65.5, 88.0, 78.5, 90.0, 61.0, 92.0]
+-----Results-----
+Lowest Grade: 61.0
+Highest Grade: 92.0
+Sum of Grades: 475.0
+Average: 79.17
+>>> 
